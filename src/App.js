ec46ba471a8d378b15a9e6bcbfafb4f17596eb54
sivaji <sivajisivaji68@gmail.com>
2021-06-11 16:06:14 +0530

    Todo-Hooks-App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo List</span> <br />
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
